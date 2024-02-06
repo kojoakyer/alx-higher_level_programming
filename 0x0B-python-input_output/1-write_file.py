cd789d95e4dbd8f2d5d3b0367e8716546800b252
kojoakyer <kojoakyer777@gmail.com>
2024-02-06 17:15:48 +0000

    updates

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+    Args:
+        filename (str): The name of file to write.
+        text (str): The text write to the file.
+    Returns:
+        The number of the characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+
